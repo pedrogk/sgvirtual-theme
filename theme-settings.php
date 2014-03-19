6dda9beaf87fc4efd6e0dba9c8d5304eb8a11166
pedrogk <pedro@sg.com.mx>
2014-03-18 21:45:10 -0500

    Updates sgvirtual.info

--- theme-settings.php
+++ theme-settings.php
@@ -7,48 +7,48 @@
 /**
  * Implements hook_form_FORM_ID_alter().
  */
-function responsive_green_form_system_theme_settings_alter(&$form, &$form_state) {
+function sgvirtual_form_system_theme_settings_alter(&$form, &$form_state) {
 
-  $form['responsive_green_settings'] = array(
+  $form['sgvirtual_settings'] = array(
     '#type' => 'fieldset',
     '#title' => t('Responsive Green Settings'),
     '#collapsible' => FALSE,
     '#collapsed' => FALSE,
   );
-  $form['responsive_green_settings']['breadcrumbs'] = array(
+  $form['sgvirtual_settings']['breadcrumbs'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show breadcrumbs in a page'),
-    '#default_value' => theme_get_setting('breadcrumbs', 'responsive_green'),
+    '#default_value' => theme_get_setting('breadcrumbs', 'sgvirtual'),
     '#description'   => t("Check this option to show breadcrumbs in page. Uncheck to hide."),
   );
-  $form['responsive_green_settings']['social'] = array(
+  $form['sgvirtual_settings']['social'] = array(
     '#type' => 'fieldset',
     '#title' => t('Social Icon'),
     '#collapsible' => TRUE,
     '#collapsed' => FALSE,
   );
-  $form['responsive_green_settings']['social']['display'] = array(
+  $form['sgvirtual_settings']['social']['display'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show Social Icon'),
-    '#default_value' => theme_get_setting('display', 'responsive_green'),
+    '#default_value' => theme_get_setting('display', 'sgvirtual'),
     '#description'   => t("Check this option to show Social Icon. Uncheck to hide."),
   );
-  $form['responsive_green_settings']['social']['twitter'] = array(
+  $form['sgvirtual_settings']['social']['twitter'] = array(
     '#type' => 'textfield',
     '#title' => t('Twitter URL'),
-    '#default_value' => theme_get_setting('twitter', 'responsive_green'),
+    '#default_value' => theme_get_setting('twitter', 'sgvirtual'),
     '#description' => t("Enter your Twitter Profile URL. example:: http://www.xyz.com"),
   );
-  $form['responsive_green_settings']['social']['facebook'] = array(
+  $form['sgvirtual_settings']['social']['facebook'] = array(
     '#type' => 'textfield',
     '#title' => t('Facebook URL'),
-    '#default_value' => theme_get_setting('facebook', 'responsive_green'),
+    '#default_value' => theme_get_setting('facebook', 'sgvirtual'),
     '#description'   => t("Enter your Facebook Profile URL. example:: http://www.xyz.com"),
   );
-  $form['responsive_green_settings']['social']['linkedin'] = array(
+  $form['sgvirtual_settings']['social']['linkedin'] = array(
     '#type' => 'textfield',
     '#title' => t('LinkedIn URL'),
-    '#default_value' => theme_get_setting('linkedin', 'responsive_green'),
+    '#default_value' => theme_get_setting('linkedin', 'sgvirtual'),
     '#description'   => t("Enter your LinkedIn Profile URL. example:: http://www.xyz.com"),
   );
 }
